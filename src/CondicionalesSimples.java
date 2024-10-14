a53fefad481e3dddedf03bdcdfdf7ea304231836
Uriel Campaña <urielcamp@gmail.com>
2024-10-14 10:57:34 -0300

    ejercicio 2

--- src/CondicionalesSimples.java
+++ src/CondicionalesSimples.java
@@ -11,7 +11,7 @@ public class CondicionalesSimples {
         double numRandom = (Math.random() * 100) - 1;
 
         System.out.println(numRandom);
-        System.out.println("---------------------");
+        System.out.println();
 
         if (numRandom > 50){
             System.out.println(numRandom + " es mayor a 50");
@@ -19,6 +19,26 @@ public class CondicionalesSimples {
             System.out.println(numRandom + " es menor a 50");
         }
 
+        System.out.println("---------------------");
+
+
+
+        //Dados dos números enteros, determinar cuál es el mayor por medio de
+        //una condicional y realizar una potencia con el número menor como
+        //exponente (Math.pow).
+
+        int num = 30;
+        int oNum = 40;
+
+        if (num > oNum){
+            System.out.println(num + " es mayor que " + oNum);
+        }else if(oNum > num) {
+            System.out.println(oNum + " es mayor que " + num);
+        }else {
+            System.out.println("los numeros son iguales");
+        }
+
+        System.out.println("---------------------");
 
 
 
