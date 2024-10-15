bb2e4c112c6f2f3c1f082a230892cea33ae969e0
Uriel Campaña <urielcamp@gmail.com>
2024-10-15 12:26:07 -0300

    ejercicio 1 bucles for

---
+++ src/BuclesFor.java
@@ -0,0 +1,34 @@
+public class BuclesFor {
+    public static void main(String[] args) {
+
+
+        //Desarrolla un programa que permita mostrar el factorial de un número
+        //definido previamente.
+
+        int numero = 5;
+        int sum = 1;
+
+        for (int i = 1; i <= numero; i++ ){
+            sum *= i;
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/CondicionalesComplejas.java
+++ src/CondicionalesComplejas.java
@@ -39,6 +39,21 @@ public class CondicionalesComplejas {
         }
 
 
+        //Un postulante a un empleo, realiza un test de capacitación, se obtuvo
+        //la siguiente información: cantidad total de preguntas que se le
+        //realizaron y la cantidad de preguntas que contestó correctamente.
+        //Desarrolle un programa que informe el nivel según el porcentaje de
+        //respuestas correctas que ha obtenido, y sabiendo que:
+        //1. Nivel máximo: Porcentaje  >= 90%.
+        //2. Nivel medio:  Porcentaje  >= 75% y <90%.
+        //3. Nivel regular:  Porcentaje  >= 50% y <75%.
+        //4. Fuera de nivel: Porcentaje  <50%.
+
+
+
+
+
+
     }
 
 }
