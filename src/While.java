6b9915f8c0fe1b6663b15967297c86c2a1e6c18d
Uriel Campaña <urielcamp@gmail.com>
2024-10-15 13:21:46 -0300

    ejercicio 1 while

---
+++ src/While.java
@@ -0,0 +1,31 @@
+public class While {
+    public static void main(String[] args) {
+
+
+
+        //Desarrollar un programa que imprima los números impares entre 1 y 25.
+
+
+        int i = 1;
+
+        while (i <= 25){
+            if (i%2 != 0){
+                System.out.println(i + " ");
+
+            }
+            i++;
+        }
+
+
+        //Desarrolla un programa que permita invertir la palabra “Programación”.
+        //El resultado debe ser “nóicamargorP”.
+
+
+
+
+
+
+
+
+    }
+}
